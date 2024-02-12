942435c5a83123a57408fc345d4244ded412b87a
sakoharu <135291091+sakoharu@users.noreply.github.com>
2024-02-12 11:06:26 +0000

    controller

--- test/controllers/postimages_controller_test.rb
+++ test/controllers/postimages_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class PostimagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get new" do
+    get postimages_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get postimages_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get postimages_show_url
+    assert_response :success
+  end
 end
