7c1087ff42843037e3405bbcc27037e33322a063
sakoharu <135291091+sakoharu@users.noreply.github.com>
2024-02-21 12:57:07 +0000

    post_image

--- app/controllers/postimages_controller.rb
+++ app/controllers/postimages_controller.rb
@@ -2,6 +2,13 @@ class PostimagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
 
   def index
   end
