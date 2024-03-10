190459f370959323f27e15713d25e8f97b363fbf
sakoharu <135291091+sakoharu@users.noreply.github.com>
2024-03-10 11:55:35 +0000

    images

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
 
-  get 'images/new,'
-  get 'images/index,'
+  get 'images/new'
+  get 'images/index'
   get 'images/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root  "homes#top"
   devise_for :users
 
+  resources :images, only: [:new, :index, :show]
 
   get "homes/about" => "homes#about", as: 'about'
 
 
-  resources :post_images, only: [:new, :create, :index, :show]
 
 
 end
